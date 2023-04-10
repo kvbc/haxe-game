4323f9f63c2df49ffe414d11024ceb33bb7ff3d3
Matt <64214044+kvbc@users.noreply.github.com>
2023-04-10 21:16:04 +0200

    Delete src directory

--- src/Extenders/FloatExtender.hx
+++
@@ -1,8 +0,0 @@
-package extenders;
-
-class FloatExtender {
-    static public function roundPrecision (number: Float, ?precision: Int = 2): Float {
-        var p10: Float = Math.pow(10, precision);
-        return Math.round(number * p10) / p10;
-    }
-}
--- src/Extenders/PosInfosExtender.hx
+++
@@ -1,9 +0,0 @@
-package extenders;
-
-import haxe.PosInfos;
-
-class PosInfosExtender {
-    static public function toString (pos: PosInfos): String {
-        return '${pos.className}:${pos.lineNumber}';
-    }
-}
